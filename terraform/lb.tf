febf379ceb779c7b2c8dfc3b8da4b127ff802fd2
Marco Palmisano <djmarco.palmisano@gmail.com>
2023-04-07 21:38:16 +0200

    fix: completed ingress lb configuration (switched to tcp for terminating tls in cluster)

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -20,7 +20,7 @@ resource "oci_load_balancer_backend_set" "lb_nodes_set_http" {
   health_checker {
     protocol          = "TCP"
     interval_ms       = 5000
-    port              = "80"
+    port              = "30208"
     retries           = 3
     timeout_in_millis = 8000
   }
@@ -34,7 +34,7 @@ resource "oci_load_balancer_backend_set" "lb_nodes_set_https" {
   health_checker {
     protocol          = "TCP"
     interval_ms       = 5000
-    port              = "80"
+    port              = "30545"
     retries           = 3
     timeout_in_millis = 8000
   }
@@ -43,17 +43,17 @@ resource "oci_load_balancer_backend_set" "lb_nodes_set_https" {
 resource "oci_load_balancer_backend" "lb_nodes_backend_http" {
   count            = 3
   backendset_name  = oci_load_balancer_backend_set.lb_nodes_set_http.name
-  ip_address       = oci_core_instance.instance[count.index+1].public_ip
+  ip_address       = oci_core_instance.instance[count.index+1].private_ip
   load_balancer_id = oci_load_balancer_load_balancer.nodes_lb.id
-  port             = "80"
+  port             = "30208"
 }
 
 resource "oci_load_balancer_backend" "lb_nodes_backend_https" {
   count            = 3
   backendset_name  = oci_load_balancer_backend_set.lb_nodes_set_https.name
-  ip_address       = oci_core_instance.instance[count.index+1].public_ip
+  ip_address       = oci_core_instance.instance[count.index+1].private_ip
   load_balancer_id = oci_load_balancer_load_balancer.nodes_lb.id
-  port             = "443"
+  port             = "30545"
 }
 
 resource "oci_load_balancer_hostname" "lb_domain_hostname" {
@@ -67,9 +67,9 @@ resource "oci_load_balancer_listener" "load_balancer_listener_http" {
   load_balancer_id         = oci_load_balancer_load_balancer.nodes_lb.id
   name                     = "${var.project_name}-lb-listener-http"
   default_backend_set_name = oci_load_balancer_backend_set.lb_nodes_set_http.name
-  hostname_names           = [oci_load_balancer_hostname.lb_domain_hostname.name]
+  hostname_names           = []
   port                     = 80
-  protocol                 = "HTTP"
+  protocol                 = "TCP"
 
   connection_configuration {
     idle_timeout_in_seconds = "240"
@@ -81,7 +81,7 @@ resource "oci_load_balancer_listener" "load_balancer_listener_https" {
   name                     = "${var.project_name}-lb-listener-https"
   default_backend_set_name = oci_load_balancer_backend_set.lb_nodes_set_https.name
   port                     = 443
-  protocol                 = "HTTP"
+  protocol                 = "TCP"
 }
 
 output "lb_public_ip" {
